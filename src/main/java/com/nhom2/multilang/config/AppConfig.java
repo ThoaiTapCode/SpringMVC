0a9ed7e929beaef45106ff5449677ddc84dc8e0b
Phan Đình Phú <127222540+phandinhphu@users.noreply.github.com>
2025-10-22 08:41:33 +0700

    minor changes

--- src/main/java/com/nhom2/multilang/config/AppConfig.java
+++ src/main/java/com/nhom2/multilang/config/AppConfig.java
@@ -6,6 +6,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "com.nhom2.multilang")
+@ComponentScan(
+    basePackages = "com.nhom2.multilang",
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ANNOTATION, value = Controller.class)
+    }
+)
 public class AppConfig {
 }
